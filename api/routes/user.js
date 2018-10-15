9ad8e8332d8e38c0cfe3c7ba2142efcbf4d76867
mateusz-ciszek <202132@edu.p.lodz.pl>
2018-10-15 21:37:40 +0200

    Logging in and returning Token

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const User = require('../models/user');
@@ -36,6 +37,43 @@ router.post('/signup', (req, res, next) => {
 				}
 			})
 		}
+	});
+});
+
+router.post('/login', (req, res, next) => {
+	User.findOne({ email: req.body.email }).exec().then(user => {
+		if (!user) {
+			return res.status(401).json({
+				message: 'Auth failed',
+			});
+		}
+		
+		bcrypt.compare(req.body.password, user.password, (err, result) => {
+			if (err) {
+				return res.status(401).json({
+					message: 'Auth failed',
+				});
+			}
+
+			if (result) {
+				const token = jwt.sign({
+					email: user.email,
+					id: user._id,
+				}, 
+				process.env.JWT_KEY, { expiresIn: "1h" });
+				return res.status(200).json({
+					message: 'Auth successful',
+					token,
+				});
+			}
+
+			return res.status(401).json({
+				message: 'Auth failed',
+			});
+		});
+	}).catch(err => {
+		console.log(err);
+		res.status(500).json({ error: err });
 	});
 });
 
