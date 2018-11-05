fee7220ef12f16c528d77f9d9f19860b6f1bacab
mateusz-ciszek <202132@edu.p.lodz.pl>
2018-11-05 19:59:41 +0100

    Zmiana wyrażeń lambda na funkcje
    Według dokumentacji Mocha lepiej i beazpieczniej jest używać funkcji

--- test/user.js
+++ test/user.js
@@ -1,20 +1,20 @@
 const mocha = require('mocha');
 const request = require('supertest');
 
-describe('User', () => {
+describe('User', function() {
 	let server, app;
-	beforeEach('Start server', (done)=> {
+	beforeEach('Start server', function(done) {
 		delete require.cache[require.resolve('../server')];
 		server = require('../server');
 		app = server.app;
 		done();
 	});
-	afterEach('Close server', () => {
+	afterEach('Close server', function() {
 		server.close();
 	})
 
-	describe('#login', () => {
-		it('sending empty request should return 401', (done) => {
+	describe('#login', function() {
+		it('sending empty request should return 401', function(done) {
 			request(app)
 				.post('/user/login')
 				.send({
