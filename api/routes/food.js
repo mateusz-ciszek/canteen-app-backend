97256cd392c96e9fc359bce863539559f7f34c80
mateusz-ciszek <202132@edu.p.lodz.pl>
2018-11-18 17:00:29 +0100

    Dodane TODO

--- api/routes/food.js
+++ api/routes/food.js
@@ -6,6 +6,7 @@ const Food = require('../models/food');
 
 module.exports = router;
 
+// TODO obsłużyć zwracanie grafiki, kiedy ta zostanie już dodana
 /**
  * GET - Pobierz posiłek o podanym ID
  * 
