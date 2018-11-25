7cadba880bc795e062dc54cb14ed3ab23bbd9595
mateusz-ciszek <202132@edu.p.lodz.pl>
2018-11-25 17:21:35 +0100

    Nie jestem pewien czy `... || false` na pewno będzie dobrze działać

--- api/routes/user.js
+++ api/routes/user.js
@@ -63,7 +63,7 @@ router.post('/login', (req, res, next) => {
 				const token = jwt.sign({
 					email: user.email,
 					_id: user._id,
-					admin: user.admin || false,
+					admin: !!user.admin,
 				}, 
 				jwtKey, { expiresIn: "1h" });
 				return res.status(200).json({
