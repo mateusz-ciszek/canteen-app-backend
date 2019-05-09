9973dcbeee6979f2eefbe43f0038adcee120ab3a
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-05-09 15:43:15 +0200

    Changed how check-role middleware is working

--- src/api/middleware/check-role.ts
+++ src/api/middleware/check-role.ts
@@ -1,12 +1,11 @@
-import { User } from '../models/user';
+import { Worker } from '../models/worker';
 import { IRequest } from '../../models/Express';
 import { NextFunction, Response } from 'express';
+import { ObjectId } from 'bson';
 
 export async function isAdmin(req: IRequest, res: Response, next: NextFunction) {
-	const user = await User.findById(req.context!.userId).exec();
-	if (!user) {
-		return res.status(404).json({ message: 'User not found' });
-	} else if (!user.admin) {
+	const worker = await Worker.find({ person: new ObjectId(req.context!.userId) }).limit(1);
+	if (!worker) {
 		return res.status(403).json();
 	}
 	next();
