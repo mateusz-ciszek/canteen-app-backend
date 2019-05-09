df6ca94ed0765b97f8b9d27cc1277b79356b3ac4
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-05-09 16:22:35 +0200

    Fix

--- src/api/middleware/check-role.ts
+++ src/api/middleware/check-role.ts
@@ -4,7 +4,7 @@ import { NextFunction, Response } from 'express';
 import { ObjectId } from 'bson';
 
 export async function isAdmin(req: IRequest, res: Response, next: NextFunction) {
-	const worker = await Worker.find({ person: new ObjectId(req.context!.userId) }).limit(1);
+	const worker = await Worker.findOne({ person: new ObjectId(req.context!.userId) });
 	if (!worker) {
 		return res.status(403).json();
 	}
