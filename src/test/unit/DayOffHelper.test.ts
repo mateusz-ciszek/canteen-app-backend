3126403e810d165c9b638631cf1bb5351574bcb1
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-07-28 00:39:11 +0200

    Commented out integration tests that are among unit tests

--- src/test/unit/DayOffHelper.test.ts
+++ src/test/unit/DayOffHelper.test.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import { expect } from 'chai';
 import { DayOffHelper } from '../../api/helper/DayOffHelper';
-import { DatabaseTestHelper } from '../testHelpers/databaseHelper';
+// import { DatabaseTestHelper } from '../testHelpers/databaseHelper';
 
 describe('DayOffHelper', () => {
 	let helper: DayOffHelper;
@@ -27,32 +27,33 @@ describe('DayOffHelper', () => {
 		});
 	});
 
-	describe('removing already existing dates function', () => {
-		const dbHelper = new DatabaseTestHelper();
+	// TODO: Move to integration tests
+	// describe('removing already existing dates function', () => {
+	// 	const dbHelper = new DatabaseTestHelper();
 	
-		before('create connection and init database', async () => {
-			await dbHelper.initDatabase();
-			await dbHelper.connect();
-		});
+	// 	before('create connection and init database', async () => {
+	// 		await dbHelper.initDatabase();
+	// 		await dbHelper.connect();
+	// 	});
 	
-		after('drop database and close connection', async () => {
-			await dbHelper.disconnect();
-			await dbHelper.dropDatabase();
-		});
+	// 	after('drop database and close connection', async () => {
+	// 		await dbHelper.disconnect();
+	// 		await dbHelper.dropDatabase();
+	// 	});
 		
-		it('should remove existing date', async () => {
-			const result = await helper.removeAlreadyRequestedDates([new Date(dbHelper.DAY_OFF.DATE)], dbHelper.DAY_OFF.WORKER);
+	// 	it('should remove existing date', async () => {
+	// 		const result = await helper.removeAlreadyRequestedDates([new Date(dbHelper.DAY_OFF.DATE)], dbHelper.DAY_OFF.WORKER);
 
-			expect(result).to.be.an('array').that.have.lengthOf(0);
-		});
+	// 		expect(result).to.be.an('array').that.have.lengthOf(0);
+	// 	});
 
-		it('should not remove unique date', async () => {
-			const date = new Date(dbHelper.DAY_OFF.DATE);
-			date.setDate(date.getDate() + 1);
+	// 	it('should not remove unique date', async () => {
+	// 		const date = new Date(dbHelper.DAY_OFF.DATE);
+	// 		date.setDate(date.getDate() + 1);
 
-			const result = await helper.removeAlreadyRequestedDates([date], dbHelper.DAY_OFF.WORKER);
+	// 		const result = await helper.removeAlreadyRequestedDates([date], dbHelper.DAY_OFF.WORKER);
 
-			expect(result).to.be.an('array').that.have.lengthOf(1);
-		});
-	});
+	// 		expect(result).to.be.an('array').that.have.lengthOf(1);
+	// 	});
+	// });
 });
