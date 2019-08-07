4f579a6f0de550dd8a6f742aaad4704906d7ba40
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-08-07 17:10:51 +0200

    Removed unused function

--- src/api/helper/mongooseErrorHelper.ts
+++ src/api/helper/mongooseErrorHelper.ts
@@ -4,13 +4,6 @@ import { CastError, Types } from 'mongoose';
 /**
  * @deprecated Use MongooseUtil instead
  */
-export function isObjectIdCastException(err: CastError): boolean {
-	return err.name === 'CastError' && err.kind === 'ObjectId';
-};
-
-/**
- * @deprecated Use MongooseUtil instead
- */
 export function isValidObjectId(id: string): boolean {
 	return Types.ObjectId.isValid(id);
 };
