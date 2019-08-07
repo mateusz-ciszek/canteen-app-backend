04ed85264b98ecb7c500b1c8204151442df3392a
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-08-07 21:24:23 +0200

    Refactored day off request validator

--- src/api/helper/validate/DayOffRequestValidator.ts
+++ src/api/helper/validate/DayOffRequestValidator.ts
@@ -1,12 +1,13 @@
 import { IWorkerDayOffRequest } from "../../interface/worker/dayOff/create/IWorkerDayOffRequest";
+import { Validator } from "./Validator";
 
-export class DayOffRequestValidator {
+export class DayOffRequestValidator extends Validator<IWorkerDayOffRequest> {
 	validate(request: IWorkerDayOffRequest): boolean {
 		if (!request.dates || !request.dates.length) {
 			return false;
 		}
 
-		let invalidDateString = request.dates.find(dateString => isNaN(Date.parse(dateString)));
+		let invalidDateString = request.dates.some(dateString => isNaN(Date.parse(dateString)));
 		if (invalidDateString) {
 			return false;
 		}
