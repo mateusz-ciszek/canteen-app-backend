7e2491b2ee94f478150d55e399d6f1d465efa8b6
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-08-09 18:56:08 +0200

    Removed unused function

--- src/api/helper/mongooseErrorHelper.ts
+++
@@ -1,9 +0,0 @@
-// TODO: remove this file when everything uses MongooseUtil
-import { CastError, Types } from 'mongoose';
-
-/**
- * @deprecated Use MongooseUtil instead
- */
-export function isValidObjectId(id: string): boolean {
-	return Types.ObjectId.isValid(id);
-};
